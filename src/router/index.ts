de128dde0235cd5e54e8f618675306144f741b0f
aaron <alkrauss48@gmail.com>
2022-02-20 16:21:12 -0600

    Add privacy policy

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,6 +14,11 @@ const routes: Array<RouteRecordRaw> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/privacy',
+    name: 'privacy',
+    component: () => import(/* webpackChunkName: "privacy" */ '../views/PrivacyView.vue')
   }
 ]
 
