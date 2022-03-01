eac85949c1119aece6a70be40050b672b7b7db40
aaron <alkrauss48@gmail.com>
2022-03-01 08:39:01 -0600

    Update home and about page content

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,45 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About CyrusLyrics</h1>
+    <p>Hi, I'm Aaron - Cyrus' dad, and the developer of CyrusLyrics.</p>
+    <p>Even since before Cyrus was born, I loved singing to him, and once he hit
+    about 2-4 months old, he really responded to that kind of interaction. But I
+    noticed that I regularly couldn't think of a big variety of songs to sing to
+    him - and even when I did, I wouldn't remember all of the lyrics.</p>
+    <p>I thought "this is crazy; I probably know at least a hundred songs and probably most of
+    the lyrics to those songs, so why can't I think of them?" That's why
+    CyrusLyrics was born. I built the app for myself so that I could easily
+    store and find the lyrics to the songs that I liked to sing to my son.</p>
+    <p>I knew that I needed the ability to add song lyrics at a whim with
+    minimal effort, so I decided to store the data in a simple Google Sheet.
+    I needed it to be dropdead easy to add and remove new songs, because every
+    day I found myself finding entirely new batches of songs that I wanted to
+    add to my list, so I made sure the app just needed an artist and song name,
+    and it found the lyrics based on that. I also wanted the ability to play those songs in Spotify, in
+    case I either didn't quite know how the songs were sung, or if I just wanted
+    to play the song in the background while I sung.</p>
+    <p>And to take the app a step further, I wanted to make it useable by other
+    people too, so everyone could create their own Google Sheet through the app
+    to start adding songs to.
+    Maybe you have a kid that you love to sing to, or maybe you just
+    like to sing in general, or maybe you're trying to better learn the lyrics
+    to some of your favorite songs. Whatever the reason is for you, I hope you
+    enjoy CyrusLyrics</p>
+    <div>
+      <a class="app-store-link" href="#">
+        <img
+          class="app-store-image"
+          alt="Download CyrusLyrics on the App Store"
+          src="../assets/download-on-the-app-store.svg"
+        />
+      </a>
+    </div>
   </div>
 </template>
+<style scoped lang="scss">
+.about {
+  max-width: 60em;
+  margin: 0 auto;
+  text-align: left;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,62 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <img class="logo" alt="CyrusLyrics logo" src="../assets/logo.jpeg">
+    <h1>Welcome to CyrusLyrics!</h1>
+    <div>
+      <a class="app-store-link" href="#">
+        <img
+          class="app-store-image"
+          alt="Download CyrusLyrics on the App Store"
+          src="../assets/download-on-the-app-store.svg"
+        />
+      </a>
+    </div>
+    <p>
+      CyrusLyrics is an iOS app to help you quickly store, find, and add the
+      lyrics of songs that you like to sing.
+    </p>
+    <p>Watch the following YouTube video to learn about how to use the app.</p>
+    <div class="yt-video">
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/pkEezNSFWtA"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+    </div>
+
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
 export default defineComponent({
   name: 'HomeView',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 })
 </script>
+
+<style scoped lang="scss">
+.logo {
+  max-width: 10em;
+}
+
+.app-store-link {
+  display: inline-block;
+}
+
+.yt-video {
+  margin: 2em 0;
+
+  iframe {
+    max-width: 100%;
+  }
+}
+</style>
